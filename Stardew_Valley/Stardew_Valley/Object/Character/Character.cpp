b14a6f3af4fa77a797267ac82b716de2de9e45ef
Saguagua <93913569+Saguagua@users.noreply.github.com>
2023-06-28 13:33:52 +0900

    캐릭터 클래스 생성 및 메인 캐릭터 리소스 추가

--- Stardew_Valley/Stardew_Valley/Object/Character/Character.cpp
+++ Stardew_Valley/Stardew_Valley/Object/Character/Character.cpp
@@ -1,2 +1,45 @@
 #include "framework.h"
 #include "Character.h"
+
+Character::Character()
+{
+	_collider = make_shared<CircleCollider>(30);
+	_quadSlot = make_shared<Transform>();
+	_quad = make_shared<Quad>(L"Resource/Player/farmer_base.png", Vector2(18, 21), Vector2(30, 50));
+	_quadSlot->SetParent(_collider->GetTransform());
+}
+
+void Character::Update()
+{
+	KeyInput();
+	_collider->Update();
+	_quadSlot->Update();
+	_quad->Update();
+}
+
+void Character::Render()
+{
+	_collider->Render();
+	_quadSlot->Set_World(0);
+	_quad->Render();
+}
+
+void Character::KeyInput()
+{
+	if (KEY_PRESS('W'))
+	{
+		_collider->AddPos(Vector2(0, 1) * DELTA_TIME * 200);
+	}
+	if (KEY_PRESS('A'))
+	{
+		_collider->AddPos(Vector2(-1, 0) * DELTA_TIME * 200);
+	}
+	if (KEY_PRESS('S'))
+	{
+		_collider->AddPos(Vector2(0, -1) * DELTA_TIME * 200);
+	}
+	if (KEY_PRESS('D'))
+	{
+		_collider->AddPos(Vector2(1, 0) * DELTA_TIME * 200);
+	}
+}
--- Stardew_Valley/Stardew_Valley/Object/Character/Character.h
+++ Stardew_Valley/Stardew_Valley/Object/Character/Character.h
@@ -3,9 +3,19 @@ class Character
 {
 public:
 
+	Character();
+	~Character() {}
+
+	void Update();
+	void Render();
+
+	void KeyInput();
+
+	shared_ptr<Transform> GetTransform() { return _collider->GetTransform(); }
 
 private:
-	shared_ptr<Transform> _transform;
-	
+	shared_ptr<Transform> _quadSlot;
+	shared_ptr<CircleCollider> _collider;
+	shared_ptr<Quad> _quad;
 };
 
