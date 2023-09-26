c6c3f6dba4643d793016551aa841d941d7eef2ce
Saguagua <93913569+Saguagua@users.noreply.github.com>
2023-09-27 00:14:50 +0900

    필요없는 부분 삭제

--- Stardew_Valley/Stardew_Valley/Framework/Render/Buffers.h
+++ Stardew_Valley/Stardew_Valley/Framework/Render/Buffers.h
@@ -184,36 +184,6 @@ struct LightColorBuffer : public ConstantBuffer
 	{
 		_data.colors[0] = sun;
 	}
-
-	void GetDarker(XMFLOAT4 val)
-	{
-		_data.colors[0].x -= val.x;
-		if (_data.colors[0].x < 0.1)
-			_data.colors[0].x = 0.1;
-
-		_data.colors[0].y -= val.y;
-		if (_data.colors[0].y < 0.1)
-			_data.colors[0].y = 0.1;
-
-		_data.colors[0].z -= val.z;
-		if (_data.colors[0].z < 0.1)
-			_data.colors[0].z = 0.1;
-	}
-
-	void GetBrighter(XMFLOAT4 val)
-	{
-		_data.colors[0].x += val.x;
-		if (_data.colors[0].x > 1.2)
-			_data.colors[0].x = 1.2;
-
-		_data.colors[0].y += val.y;
-		if (_data.colors[0].y > 1.2)
-			_data.colors[0].y = 1.2;
-
-		_data.colors[0].z += val.z;
-		if (_data.colors[0].z > 1.2)
-			_data.colors[0].z = 1.2;
-	}
 	
 private:
 	struct Data
